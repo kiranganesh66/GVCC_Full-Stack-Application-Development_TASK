162d461eaa02f5c94e46a51f124f8ed7949346b2
kiran66 <kiranganesh66@gmail.com>
2024-12-29 21:51:01 +0530

    Initial commit

---
+++ BckEnd/models/User.js
@@ -0,0 +1,27 @@
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user", // default role
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
